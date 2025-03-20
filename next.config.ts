a5f34a1da4adbd64db5a5f2868de1eb0da88ab0b
Pedro Castro <pedro.tiago.castro.ist@gmail.com>
2025-03-20 18:36:35 +0000

    feat: created .env.local which is not commited and added env variables from it to next.js application

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  env: {
+    NEXT_PUBLIC_GOOGLE_MAPS_API_KEY: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
+    DELAY_THRESHOLD_MINUTES: process.env.DELAY_THRESHOLD_MINUTES,
+  },
 };
 
 export default nextConfig;
